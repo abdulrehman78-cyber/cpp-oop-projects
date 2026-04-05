5b2723d497db3cedbd640a6701b0f8cd00c886d9
abdduuoo <abdduuoo76487336@gmail.com>
2026-04-05 11:46:48 +0500

    changes

--- librarySystem/book.hpp
+++ librarySystem/book.hpp
@@ -2,70 +2,58 @@
 #define BOOK_H
 #include <iostream>
 using namespace std;
-class Book
-{
-protected:
+class Book{
+    protected:
     string name;
     string author;
-
-public:
-    Book()
-    {
+    public:
+    Book(){
         name = "";
         author = "";
     };
-    Book(string name, string author)
-    {
+    Book(string name,string author){
         this->name = name;
         this->author = author;
     }
 
-    void display()
-    {
-        cout << "Name : " << name << endl
-             << "Author : " << author << endl;
+    void display(){
+        cout<<"Name : "<<name<<endl<<
+        "Author : "<<author<<endl;
     }
 
-    string getName()
-    {
+    string getName(){
         return name;
     }
 
-    bool operator==(Book &obj)
-    {
-        return (obj.name == name && obj.author == author);
+    bool operator==(Book &obj){
+        return  (obj.name == name && obj.author == author); 
     }
 
     bool Issued = false;
 
-    void issue()
-    {
+    void issue(){
         Issued = true;
     }
 
-    bool getIsIssued()
-    {
+    bool getIsIssued(){
         return Issued;
     }
-    string getAuthor()
-    {
+    string getAuthor(){
         return author;
     }
-    string getBook()
-    {
+    string getBook(){
         return name;
     }
 
     string issuedTo = "";
 
-    void setIssuedTo(string memberName)
-    {
+    void setIssuedTo(string memberName){
         issuedTo = memberName;
     }
 
-    string getIssuedTo()
-    {
+    string getIssuedTo(){
         return issuedTo;
     }
+    
 };
 #endif
